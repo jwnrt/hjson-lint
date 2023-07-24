1e49df4210cca38771e8f37f8877dd958960bbba
James Wainwright <james.wainwright@lowrisc.org>
2023-07-24 10:18:22 +0100

    Add boolean parser

--- src/boolean.rs
+++ src/boolean.rs
@@ -5,6 +5,26 @@ pub struct Boolean;
 
 impl Parse for Boolean {
     fn parse(input: &str) -> Option<Token> {
-        todo!()
+        if input.starts_with("true") {
+            Some(Token::new(Boolean, 4))
+        } else if input.starts_with("false") {
+            Some(Token::new(Boolean, 5))
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn boolean() {
+        assert_eq!(Boolean::parse("true"), Some(Token::new(Boolean, 4)));
+        assert_eq!(Boolean::parse("false"), Some(Token::new(Boolean, 5)));
+        assert_eq!(Boolean::parse("true "), Some(Token::new(Boolean, 4)));
+        assert_eq!(Boolean::parse(" true"), None);
+        assert_eq!(Boolean::parse(""), None);
     }
 }
