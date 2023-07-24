61497eb6f174c0ec20861142a079effba54cf34d
James Wainwright <james.wainwright@lowrisc.org>
2023-07-24 10:24:54 +0100

    Add symbol parser

--- src/symbol.rs
+++ src/symbol.rs
@@ -12,6 +12,45 @@ pub enum Symbol {
 
 impl Parse for Symbol {
     fn parse(input: &str) -> Option<Token> {
-        todo!()
+        let symbol = match input.chars().next()? {
+            '{' => Symbol::OpenBrace,
+            '}' => Symbol::CloseBrace,
+            '[' => Symbol::OpenBracket,
+            ']' => Symbol::OpenBracket,
+            ':' => Symbol::Colon,
+            ',' => Symbol::Comma,
+            _ => return None,
+        };
+
+        Some(Token::new(symbol, 1))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn valid() {
+        let symbols = [
+            ("{", Symbol::OpenBrace),
+            ("}", Symbol::CloseBrace),
+            ("[", Symbol::OpenBracket),
+            ("]", Symbol::OpenBracket),
+            (":", Symbol::Colon),
+            (",", Symbol::Comma),
+        ];
+
+        for (s, symbol) in symbols {
+            assert_eq!(Symbol::parse(s), Some(Token::new(symbol, 1)));
+        }
+    }
+
+    #[test]
+    fn invalid() {
+        let invalid = ["!", " {", "x"];
+        for invalid in invalid {
+            assert_eq!(Symbol::parse(invalid), None);
+        }
     }
 }
