2c67f764193b64e78c76b4c1228522647be63381
James Wainwright <james.wainwright@lowrisc.org>
2023-07-24 11:48:01 +0100

    Add whitespace parser

--- src/whitespace.rs
+++ src/whitespace.rs
@@ -8,6 +8,43 @@ pub enum Whitespace {
 
 impl Parse for Whitespace {
     fn parse(input: &str) -> Option<Token> {
-        todo!()
+        if input.starts_with('\n') {
+            return Some(Token::new(Whitespace::NewLine, 1));
+        }
+
+        let non_whitespace = input
+            .find(|c: char| c == '\n' || !c.is_whitespace())
+            .unwrap_or(input.len());
+
+        match non_whitespace {
+            0 => None,
+            len => Some(Token::new(Whitespace::Other, len)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn whitespace() {
+        assert_eq!(
+            Whitespace::parse(" "),
+            Some(Token::new(Whitespace::Other, 1))
+        );
+        assert_eq!(
+            Whitespace::parse("\t"),
+            Some(Token::new(Whitespace::Other, 1))
+        );
+        assert_eq!(
+            Whitespace::parse("\n"),
+            Some(Token::new(Whitespace::NewLine, 1))
+        );
+        assert_eq!(
+            Whitespace::parse(" \t\n"),
+            Some(Token::new(Whitespace::Other, 2))
+        );
+        assert_eq!(Whitespace::parse("a \t\n"), None);
     }
 }
